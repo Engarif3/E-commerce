c73ca797482dd475e457252fd4df7ab1cf549907
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-06 22:30:04 +0200

    product card size adjusted

--- E-commerce/src/component/NavBar.jsx
+++ E-commerce/src/component/NavBar.jsx
@@ -4,7 +4,7 @@ import logo from "../../public/shop.svg"
 const NavBar = () => {
     return (
         <div className='bg-cyan-800 text-white'>
-            <nav className='flex justify-between items-center mx-28 mb-2 '>
+            <nav className='flex justify-between items-center mx-28 p-2'>
             <img className='w-12' src={logo}  alt="" />
             <div className='flex gap-12 font-bold'>
             <p className='cursor-pointer hover:text-orange-700'>Order</p>
