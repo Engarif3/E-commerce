6a8f5d0b4e1395a3927b81e267490f08e5575cb8
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-11 18:09:21 +0200

    code formatted

--- E-commerce/src/component/NavBar.jsx
+++ E-commerce/src/component/NavBar.jsx
@@ -8,10 +8,12 @@ const NavBar = () => {
             <nav className='flex justify-between items-center mx-28 p-2'>
             <img className='w-12' src={logo}  alt="" />
             <div className='flex gap-12 font-bold'>
-            <p className='cursor-pointer hover:text-orange-700'>Order</p>
+
+            <p className='cursor-pointer hover:text-orange-700'> Order </p>
             <p className='cursor-pointer hover:text-orange-700'>Order Review</p>
             <p className='cursor-pointer hover:text-orange-700'>Manage Inventory</p>
             <p className='cursor-pointer hover:text-orange-700'>Login</p>
+
             </div>
         </nav>
         </div>
