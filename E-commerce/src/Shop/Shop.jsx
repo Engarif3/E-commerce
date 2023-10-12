8fee68d099bf3f2461c90f9f5b23bcd0d6057d4c
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-12 21:57:41 +0200

    Shop component code formatted

--- E-commerce/src/Shop/Shop.jsx
+++ E-commerce/src/Shop/Shop.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import Product from "../Products/Product";
-import "./Shop.css"
+import "./Shop.css";
 import OrderSummary from "../component/OrderSummary";
-
 
 function Shop() {
   const [products, setProducts] = useState([]);
-  const [cart, setCart]= useState();
+  const [cart, setCart] = useState();
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -15,25 +14,33 @@ function Shop() {
   }, []);
 
   const handleAddToCart = () => {
-    console.log("product added")
-    setCart("mission done")
-  }
-  
+    console.log("product added");
+    setCart("mission done");
+  };
+
   return (
-    <div className="flex justify-center "> 
+    <div className="flex justify-center ">
+
       <div>
-        <h2 className="text-center text-5xl">Total Products: {products.length}</h2>
+        <h2 className="text-center text-5xl">
+          Total Products: {products.length}
+        </h2>
         <div className="shop-container">
           <div className="grid grid-cols-4 gap-12 my-12">
-          {products.map((product) => (
-            <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>
-          ))}
+            {products.map((product) => (
+              <Product
+                key={product.id}
+                product={product}
+                handleAddToCart={handleAddToCart}
+              ></Product>
+            ))}
           </div>
           <div className="bg-orange-200 my-12 rounded-md">
             <OrderSummary cart={cart}></OrderSummary>
           </div>
         </div>
       </div>
+      
     </div>
   );
 }
